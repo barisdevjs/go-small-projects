a7c6fcb0e7b3532deb264c10f1e9c6e65a88f5b3
Barış SAVAŞ <barissavas17@gmail.com>
2023-05-26 00:24:58 +0300

    db changed to mysql to postgresql

--- bookstore/cmd/main/main.go
+++ bookstore/cmd/main/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bookstore/pkg/config"
 	"bookstore/pkg/routes"
-	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
+	config.Connect() // Establish the PostgreSQL database connection
+
 	r := mux.NewRouter()
 	routes.RegisterBookStore(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	log.Fatal(http.ListenAndServe(":8080", r)) // Start the HTTP server on port 8080
+	fmt.Print("STARTED")
+
 }
