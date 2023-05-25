dfbd57ce4059d2e2daeef1eee3c6b0ec4e7de573
Barış SAVAŞ <barissavas17@gmail.com>
2023-05-25 21:11:05 +0300

    main.go added

---
+++ bookstore/cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStore(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
