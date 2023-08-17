49abf076ce5be1b63e69adb11cc5db1860a98e57
Barış SAVAŞ <barissavas17@gmail.com>
2023-08-17 23:03:19 +0300

    utils added

--- bookstore/pkg/utils/utils.go
+++ bookstore/pkg/utils/utils.go
@@ -2,13 +2,13 @@ package utils
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal(body, x); err != nil {
 			return
 		}
 	}
