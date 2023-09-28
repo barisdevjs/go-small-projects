1fc514dd406b85d2770c1a5bfdb3f808b03a069b
Barış SAVAŞ <barissavas17@gmail.com>
2023-09-28 23:15:23 +0300

    auth logic added

---
+++ rssagg/middleware_auth.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"rssagg/internal/auth"
+	"rssagg/internal/database"
+)
+
+type authHandler func(http.ResponseWriter, *http.Request, database.User)
+
+func (apiCfg *apiConfig) middlewareAuth(handler authHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey, err := auth.GetAPIKey(r.Header)
+		if err != nil {
+			respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+			return
+		}
+
+		user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apiKey)
+		if err != nil {
+			respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+			return
+		}
+		handler(w, r, user)
+	}
+}
