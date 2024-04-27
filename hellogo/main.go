849b96b24824295ace767d7a1a126684d6cbf12b
Barış SAVAŞ <barissavas17@gmail.com>
2024-04-27 21:32:14 +0300

    hello go changed

--- hellogo/main.go
+++ hellogo/main.go
@@ -1,10 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"github.com/barisdevjs/mystrings"
-)
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
 
 func main() {
-	fmt.Println(mystrings.Reverse("Hello World"))
+	fmt.Println(fact(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
 }
